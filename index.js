8dcdd61b7676d32acfa4da9d9623490fef9e94ea
funtee7 <talabifunmilayo@gmail.com>
2021-05-29 01:42:30 +0100

    adjusting styless for content

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const form = document.querySelector(".input-section");
 const getProfileDetails = (username) =>
   `{
     repositoryOwner(login: "${username}"){
+      login
     ... on User{
       bio
       avatarUrl
+    }
+    ... on ProfileOwner{
+      name
     }
   }
     user(login: "${username}"){
@@ -15,6 +19,7 @@ const getProfileDetails = (username) =>
         name
         forkCount
         url
+        shortDescriptionHTML
         languages(first: 1){
           nodes{
             name
@@ -57,42 +62,83 @@ const appendData = (data) => {
   while (profileContainer.firstChild) {
     profileContainer.removeChild(profileContainer.firstChild);
   }
-  const bio = document.createElement("div");
+  const leftSegment = document.createElement("div");
+  const userName = document.createElement("p");
+  const fullName = document.createElement("h2");
+  const bio = document.createElement("p");
+  const avaterContainer = document.createElement("div");
   const avatar = document.createElement("img");
-  const repoCards = document.createElement("div");
-  const repoFragment = document.createDocumentFragment();
+  const rightSegment = document.createElement("section");
+
   bio.textContent = data.repositoryOwner.bio;
   avatar.setAttribute("src", data.repositoryOwner.avatarUrl);
+  fullName.textContent = data.repositoryOwner.name;
+  userName.textContent = data.repositoryOwner.login;
 
-  profileContainer.appendChild(bio);
-  profileContainer.appendChild(avatar);
+  avaterContainer.appendChild(avatar);
+  leftSegment.appendChild(avaterContainer);
+  leftSegment.appendChild(fullName);
+  leftSegment.appendChild(userName);
+  leftSegment.appendChild(bio);
+  profileContainer.appendChild(leftSegment);
 
-  // console.log(data.user.repositories.nodes);
+  leftSegment.classList.add("left-segment");
+  avaterContainer.classList.add("image-container");
+  userName.classList.add("username-text");
   data.user.repositories.nodes.forEach((repo) => {
+    const repoCards = document.createElement("div");
+    const lowerCardContent = document.createElement("div");
     const forkCount = document.createElement("p");
     const repoName = document.createElement("a");
     const updatedAt = document.createElement("p");
     const favouriteCount = document.createElement("p");
-    // const mainLan = document.createElement("p");
+    const description = document.createElement("p");
+    const mainLan = document.createElement("p");
+    const languageColor = document.createElement("p");
 
     forkCount.textContent = repo.forkCount;
     repoName.textContent = repo.name;
     updatedAt.textContent = repo.pushedAt;
     favouriteCount.textContent = repo.stargazerCount;
     repoName.href = repo.url;
+    description.textContent = repo.shortDescriptionHTML;
+
     // mainLan.textContent = mainLanguageA || "nothing";
 
-    repo.languages.nodes.forEach((lang) => {
-      const mainLan = document.createElement("p");
-      mainLan.textContent = lang.name;
-      repoCards.appendChild(mainLan);
-    });
-    repoCards.appendChild(forkCount);
     repoCards.appendChild(repoName);
-    repoCards.appendChild(updatedAt);
-    repoCards.appendChild(favouriteCount);
+    repoCards.appendChild(description);
+    lowerCardContent.appendChild(languageColor);
+    repo.languages.nodes.forEach((lang) => {
+      mainLan.textContent = lang.name;
+      lowerCardContent.appendChild(mainLan);
+    });
+    lowerCardContent.appendChild(favouriteCount);
+    lowerCardContent.appendChild(forkCount);
+    lowerCardContent.appendChild(updatedAt);
+
+    repoCards.appendChild(lowerCardContent);
+    rightSegment.appendChild(repoCards);
+
+    languageColor.classList.add("language-color");
+    const languageText = mainLan.innerHTML;
+
+    console.log(languageText);
+    if (languageText === "HTML") {
+      languageColor.style.backgroundColor = "#e34c26";
+    } else if (languageText === "JavaScript") {
+      languageColor.style.backgroundColor = "#f1e05a";
+    } else if (languageText === "CSS") {
+      languageColor.style.backgroundColor = "#563d7c";
+    } else if (languageText === "Python") {
+      languageColor.style.backgroundColor = "#3572a5";
+    }
+
+    repoCards.classList.add("repo-card");
+    lowerCardContent.classList.add("lower-card-content");
   });
 
-  repoFragment.appendChild(repoCards);
-  profileContainer.appendChild(repoFragment);
+  // rightSegment.appendChild(repoContainer);
+  profileContainer.appendChild(rightSegment);
+
+  rightSegment.classList.add("right-segment");
 };
